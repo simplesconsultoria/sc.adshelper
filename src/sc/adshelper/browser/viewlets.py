ab508acb4c45c8f7a8bf5e855c4c73c115553371
hvelarde <hector.velarde@gmail.com>
2015-04-10 11:32:20 -0300

    Add placehlders for code and a condition to show viewlets only to anonymous users

--- src/sc/adshelper/browser/viewlets.py
+++ src/sc/adshelper/browser/viewlets.py
@@ -5,37 +5,45 @@ from plone.app.layout.viewlets.common import ViewletBase
 from sc.adshelper.config import BASE_REGISTRY
 
 
-class HtmlHeadViewlet(ViewletBase):
+class AdsHelperViewletBase(ViewletBase):
+
+    """Base class for Ads Helper viewlets."""
+
+    def show(self):
+        return api.user.is_anonymous()
+
+
+class HtmlHeadViewlet(AdsHelperViewletBase):
 
     """Viewlet to be rendered inside the head tag."""
 
-    def render(self):
-        """Return the content of the html_head record."""
-        return api.portal.get_registry_record(BASE_REGISTRY + 'html_head')
+    def update(self):
+        """Update viewlet with the content of the html_head record."""
+        self.code = api.portal.get_registry_record(BASE_REGISTRY + 'html_head')
 
 
-class AboveContentViewlet(ViewletBase):
+class AboveContentViewlet(AdsHelperViewletBase):
 
     """Viewlet to be rendered above the content of the page."""
 
-    def render(self):
-        """Return the content of the above_content record."""
-        return api.portal.get_registry_record(BASE_REGISTRY + 'above_content')
+    def update(self):
+        """Update viewlet with the content of the above_content record."""
+        self.code = api.portal.get_registry_record(BASE_REGISTRY + 'above_content')
 
 
-class BelowContentViewlet(ViewletBase):
+class BelowContentViewlet(AdsHelperViewletBase):
 
     """Viewlet to be rendered below the content of the page."""
 
-    def render(self):
-        """Return the content of the below_content record."""
-        return api.portal.get_registry_record(BASE_REGISTRY + 'below_content')
+    def update(self):
+        """Update viewlet with the content of the below_content record."""
+        self.code = api.portal.get_registry_record(BASE_REGISTRY + 'below_content')
 
 
-class FooterViewlet(ViewletBase):
+class FooterViewlet(AdsHelperViewletBase):
 
     """Viewlet to be rendered at the end of the page."""
 
-    def render(self):
-        """Return the content of the footer record."""
-        return api.portal.get_registry_record(BASE_REGISTRY + 'footer')
+    def update(self):
+        """Update viewlet with the content of the footer record."""
+        self.code = api.portal.get_registry_record(BASE_REGISTRY + 'footer')
