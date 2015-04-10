03d3c2cb647e9218f59bfe7f14d59e94c65795ed
hvelarde <hector.velarde@gmail.com>
2015-04-10 17:05:36 -0300

    Set default size for control panel configlet fields as 80x10

--- src/sc/adshelper/interfaces.py
+++ src/sc/adshelper/interfaces.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from plone.directives import form
+from plone.supermodel import model
 from sc.adshelper import _
 from zope import schema
 from zope.interface import Interface
@@ -10,7 +11,7 @@ class IBrowserLayer(Interface):
     """A layer specific for this add-on product."""
 
 
-class IAdsHelperSettings(form.Schema):
+class IAdsHelperSettings(model.Schema):
 
     """Schema for the control panel form."""
 
@@ -23,6 +24,7 @@ class IAdsHelperSettings(form.Schema):
         default=False,
     )
 
+    form.widget('html_head', cols=80, rows=10)
     html_head = schema.Text(
         title=_(u'HTML Head'),
         description=_(u'This code will be included inside the head tag.'),
@@ -30,6 +32,7 @@ class IAdsHelperSettings(form.Schema):
         required=False,
     )
 
+    form.widget('header', cols=80, rows=10)
     header = schema.Text(
         title=_(u'Header'),
         description=_(u'This code will be included at the top of the page.'),
@@ -37,6 +40,7 @@ class IAdsHelperSettings(form.Schema):
         required=False,
     )
 
+    form.widget('above_content', cols=80, rows=10)
     above_content = schema.Text(
         title=_(u'Above Content'),
         description=_(u'This code will be included above the content of the page.'),
@@ -44,6 +48,7 @@ class IAdsHelperSettings(form.Schema):
         required=False,
     )
 
+    form.widget('below_content_body', cols=80, rows=10)
     below_content_body = schema.Text(
         title=_(u'Below Content Body'),
         description=_(u'This code will be included below the body of the content of the page.'),
@@ -51,6 +56,7 @@ class IAdsHelperSettings(form.Schema):
         required=False,
     )
 
+    form.widget('below_content', cols=80, rows=10)
     below_content = schema.Text(
         title=_(u'Below Content'),
         description=_(u'This code will be included below the content of the page.'),
@@ -58,6 +64,7 @@ class IAdsHelperSettings(form.Schema):
         required=False,
     )
 
+    form.widget('footer', cols=80, rows=10)
     footer = schema.Text(
         title=_(u'Footer'),
         description=_(u'This code will be included at the end of the page.'),
