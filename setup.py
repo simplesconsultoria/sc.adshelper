c59e81a388d466a754381f7b43ea123d8e99cc6c
hvelarde <hector.velarde@gmail.com>
2015-04-13 12:53:47 -0300

    Back to development: 1.0a2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0a1'
+version = '1.0a2.dev0'
 description = 'Helper viewlets to easily insert code on the layout of a Plone site.'
 long_description = (
     open('README.rst').read() + '\n' +
