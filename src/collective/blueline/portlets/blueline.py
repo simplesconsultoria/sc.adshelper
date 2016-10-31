f334137198a2b8698743810184e6ecdeb98f99a1
hvelarde <hector.velarde@gmail.com>
2016-10-31 19:05:16 -0200

    Clean up

--- src/collective/blueline/portlets/blueline.py
+++ src/collective/blueline/portlets/blueline.py
@@ -54,10 +54,7 @@ class Assignment(base.Assignment):
     title = None
     description = None
 
-    def __init__(self,
-                 embed=None,
-                 title=None,
-                 description=None):
+    def __init__(self, embed=None, title=None, description=None):
         self.embed = embed
         self.title = title
         self.description = description
@@ -67,17 +64,12 @@ class Renderer(base.Renderer):
 
     render = ViewPageTemplateFile('blueline.pt')
 
-    def __init__(self, *args):
-        base.Renderer.__init__(self, *args)
-
 
 class AddForm(base.AddForm):
 
     form_fields = form.Fields(IBluelinePortlet)
-
     label = _(u'Add Blueline Portlet')
-    description = _(u'blueline_portlet_description',
-                    default=u'This portlet embed content from remote source.')
+    description = _(u'This portlet embeds content from remote source.')
 
     def create(self, data):
         return Assignment(**data)
@@ -86,7 +78,5 @@ class AddForm(base.AddForm):
 class EditForm(base.EditForm):
 
     form_fields = form.Fields(IBluelinePortlet)
-
     label = _(u'Edit Blueline Portlet')
-    description = _('blueline_portlet_description',
-                    default=u'This portlet embed content from remote source.')
+    description = _(u'This portlet embeds content from remote source.')
